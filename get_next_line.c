28d585b7e592f59987ec81617d360c6750c7a012
mousliiim <mouslim.mourdalov@gmail.com>
2022-10-20 19:25:01 +0200

    GNL First

---
+++ get_next_line.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmourdal <mmourdal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 14:40:50 by mmourdal          #+#    #+#             */
+/*   Updated: 2022/10/20 14:53:13 by mmourdal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	int		i;
+	char	*readstr;
+	char	*previous;
+
+	if (!BUFFER_SIZE || BUFFER_SIZE < 1 || fd < 0 || read(fd, 0, 0) < 0)
+		return (0);
+}
---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmourdal <mmourdal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 14:43:34 by mmourdal          #+#    #+#             */
+/*   Updated: 2022/10/20 14:52:59 by mmourdal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE	42
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
