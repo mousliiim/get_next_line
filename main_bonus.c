90c1bf869287f110f13383f1a95fa8ff0e39120c
mousliiim <mouslimby77@gmail.com>
2022-11-24 01:11:04 +0100

    Main Bonus et Text

--- main_bonus.c
+++ main_bonus.c
@@ -1,39 +1,54 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main_mandatory.c                                   :+:      :+:    :+:   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmourdal <mmourdal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 23:40:50 by mmourdal          #+#    #+#             */
-/*   Updated: 2022/11/24 00:48:52 by mmourdal         ###   ########.fr       */
+/*   Updated: 2022/11/24 01:06:27 by mmourdal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int	main(int argc, char **argv)
 {
-	int		fd;
+	int		fd0;
+	int		fd1;
+	int		fd2;
 	char	*s;
 	int		max;
 	int		i;
 
-	if (argc != 3)
+	if (argc != 5)
 	{
-		printf("./a.out [Nombre de Ligne] [File]\n");
+		printf("./a.out [Nombre de Ligne] [File 1] [File 2] [File 3]\n");
 		return (0);
 	}
 	max = atoi(argv[1]);
-	fd = open(argv[2], O_RDONLY);
+	fd0 = open(argv[2], O_RDONLY);
+	fd1 = open(argv[3], O_RDONLY);
+	fd2 = open(argv[4], O_RDONLY);
 	i = 0;
 	printf("\n********** GET NEXT LINE : **********\n\n");
 	while (++i <= max)
 	{
-		s = get_next_line(fd);
-		printf("**\t[%d Appel] ==> %s        \t\t\t   **\n", i, s);
+		s = get_next_line(fd0);
+		printf("fd0\t[%d Appel] ==> %s        \t\t\t   **\n", i, s);
+		free(s);
+		s = get_next_line(fd1);
+		printf("fd1\t[%d Appel] ==> %s        \t\t\t   **\n", i, s);
+		free(s);
+		s = get_next_line(fd2);
+		printf("fd2\t[%d Appel] ==> %s        \t\t\t   **\n", i, s);
 		free(s);
 	}
 	printf("\n*************************************\n\n");
+	close(fd0);
+	close(fd1);
+	close(fd2);
 }
